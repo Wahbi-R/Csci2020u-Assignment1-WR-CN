a55c6caa903ea7a09539e6719fc8c483b0358a2b
Wahbi <wahbi.raihan@ontariotechu.net>
2021-03-08 00:53:05 -0500

    adding main file

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] run = new String[2];
+        run[0] = "temp";
+        run[1] = "temp1.txt";
+        WordCounter.main(run);
+    }
+}
---
+++ out/production/Csci2020u-Assignment1-WR-CN/README.md
@@ -0,0 +1,3 @@
+This is assignment 1 for CSCI2020U. 
+
+Group Members: Wahbi Raihan & Chau Nguyen
