db7f13367c156eb2af13cd4308f1e5f0da0c4c11
Chau Nguyen <chau.nguyen@ontariotechu.net>
2021-03-08 12:32:32 +0700

    modify the WordCounter to count the files a word shows up

---
+++ WordCounter.java
@@ -0,0 +1,125 @@
+import java.io.*;
+import java.util.*;
+
+
+public class WordCounter{
+	
+	private Map<String, Integer> wordCounts;
+	private Map<String, Integer> trainHamFreq;
+	private Map<String, Integer> trainSpamFreq;
+	
+	public WordCounter(){
+		wordCounts = new TreeMap<>();
+		trainHamFreq = new TreeMap<>();
+		trainSpamFreq = new TreeMap<>();
+	}
+	
+	public void parseFile(File file) throws IOException{
+		System.out.println("Starting parsing the file:" + file.getAbsolutePath());
+		HashSet<String> existingWords = new HashSet<String>();
+		if(file.isDirectory()){
+			//parse each file inside the directory
+			File[] content = file.listFiles();
+			for(File current: content){
+				parseFile(current);
+			}
+		}else{
+			existingWords.clear();
+			Scanner scanner = new Scanner(file);
+			// scanning token by token
+			while (scanner.hasNext()){
+				String  token = scanner.next();
+				if (isValidWord(token)){
+					existingWords.add(token);
+				}
+			}
+			ArrayList<String> words = new ArrayList<String>();
+			for(String i: existingWords){
+				words.add(i);
+			}
+			
+			for(int i=0;i< words.size();i++){
+				if(isValidWord(words.get(i)) && wordCounts.containsKey(words.get(i))){
+					int previous = wordCounts.get(words.get(i));
+					wordCounts.put(words.get(i), previous+1);
+				}
+				else{
+					wordCounts.put(words.get(i),1);
+				}
+			}	
+		}		
+		
+	}
+	
+	private boolean isValidWord(String word){
+		String allLetters = "^[a-zA-Z]+$";
+		// returns true if the word is composed by only letters otherwise returns false;
+		return word.matches(allLetters);
+			
+	}
+	
+	private void countWord(String word){
+		if(wordCounts.containsKey(word)){
+			int previous = wordCounts.get(word);
+			wordCounts.put(word, previous+1);
+		}else{
+			wordCounts.put(word, 1);
+		}
+	}
+	
+	public void outputWordCount(int minCount, File output) throws IOException{
+		System.out.println("Saving word counts to file:" + output.getAbsolutePath());
+		System.out.println("Total words:" + wordCounts.keySet().size());
+		
+		if (!output.exists()){
+			output.createNewFile();
+			if (output.canWrite()){
+				PrintWriter fileOutput = new PrintWriter(output);
+				
+				Set<String> keys = wordCounts.keySet();
+				Iterator<String> keyIterator = keys.iterator();
+				
+				while(keyIterator.hasNext()){
+					String key = keyIterator.next();
+					int count = wordCounts.get(key);
+					// testing minimum number of occurances
+					if(count>=minCount){					
+						fileOutput.println(key + ": " + count);
+					}
+				}
+				
+				fileOutput.close();
+			}
+		}else{
+			System.out.println("Error: the output file already exists: " + output.getAbsolutePath());
+		}
+		
+	}
+	
+	//main method
+	public static void main(String[] args) {
+		
+		if(args.length < 2){
+			System.err.println("Usage: java WordCounter <inputDir> <outfile>");
+			System.exit(0);
+		}
+		
+		File dataDir = new File(args[0]);
+		File outFile = new File(args[1]);		
+		
+		WordCounter wordCounter = new WordCounter();
+		System.out.println("Hello");
+		try{
+			wordCounter.parseFile(dataDir);
+			wordCounter.outputWordCount(1, outFile);
+		}catch(FileNotFoundException e){
+			System.err.println("Invalid input dir: " + dataDir.getAbsolutePath());
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
